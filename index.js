434510b5ddd215bbe4a3f3399c3cb702813d28f1
Ho Yeung (Peter) Chan <peterhyportfolio@gmail.com>
2020-05-02 09:33:09 -0700

    list added and deployed

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+const PROJECT_LIST= require("./projects.js");
+
+app.listen(PORT, ()=> {
+	console.log(`Running on Port ${PORT}.`);
+});
+
+app.get("/", (req,res)=>{
+	return res.send(PROJECT_LIST);
+});
+
